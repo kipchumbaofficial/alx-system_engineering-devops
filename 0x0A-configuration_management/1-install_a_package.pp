0e0811717b9ecc34de64de91dfc19c10d7119d36
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-19 16:53:54 +0300

    TAsk 1: install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # install a package using puppet
-package {'flask':
+
+package { 'flask':
     ensure   => '2.1.0',
     provider => 'pip3',
-    require  => Package['python3-pip'],
+}
+package { 'werkzeug':
+    ensure   => '2.1.1',
+    provider => 'pip3',
 }
