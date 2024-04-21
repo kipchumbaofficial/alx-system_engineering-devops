2791a5602f1b613a12e576cb33070511eaf7c474
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-21 11:33:18 +0300

    Task 4: More corrections

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Puppet file to edit a config
 
-file_line {'PasswordAuth':
-    ensure => present,
-    path   => '/etc/ssh/sshd_config',
+file_line { 'Turn off password auth':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
     line   => '    PasswordAuthentication no',
 }
 
-file_line {'Set private':
-    ensure => present,
-    path   => '/etc/ssh/sshd_config',
+file_line { 'Declaring identity file':
+    ensure => 'present',
+    path   => '/etc/ssh/ssh_config',
     line   => '    IdentityFile ~/.ssh/school',
 }
