06af967e42c7a17d30f18fe24fd0718cbbef0bb1
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-04-21 09:07:30 +0300

    Task 4: correction

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 # Puppet file to edit a config
 
-file {'/home/kipchumba/.ssh/config':
-    ensure  => present,
-    content => "Host 107.22.144.84\n    IdentityFile ~/.ssh/school\n    PasswordAuthentication no",
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/holberton" >> /etc/ssh/ssh_config':
+        path    => '/bin/'
 }
