bdc4ef2f0dadf2af0d2269a448567654417284e5
Victor Kipchumba <victorkipchumba039@gmail.com>
2024-06-04 15:49:40 +0300

    Task 0: Correction#2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,11 +10,15 @@ def number_of_subscribers(subreddit):
     or 0 if the subreddit is invalid'''
 
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    user = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; '
+    user += 'rv:114.0) Gecko/20100101 Firefox/114.0'
+    headers = {
+            'User-Agent': user
+            }
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    response = requests.get(url)
-    retval = response.json()
-
-    if 'data' in retval and 'subscribers' in retval['data']:
+    if response.status_code == 200:
+        retval = response.json()
         return retval['data']['subscribers']
     else:
         return 0
